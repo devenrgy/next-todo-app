dbe2836ebffbd113270a578e8bc0ca5455cb0f20
Alex <adabiralexander@gmail.com>
2023-12-10 03:03:55 +0300

    feat: add background

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={'container'}>
       <div className={'flex justify-between py-28'}>
-        <h1 className={'text-4xl uppercase tracking-widest font-bold'}>Todo</h1>
+        <h1 className={'text-4xl uppercase tracking-widest font-bold text-white'}>Todo</h1>
         <ModeToggle />
       </div>
     </main>
