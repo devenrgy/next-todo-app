c7ddf8aa5afb62651edab2e7fef9e264903566d3
Alex <adabiralexander@gmail.com>
2023-12-10 13:49:58 +0300

    feat: add todos controls

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ModeToggle } from '@/components/theme-toggle'
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Toggle } from '@/components/ui/toggle'
@@ -7,25 +8,37 @@ export default function Home() {
   return (
     <main className={'container'}>
       <div className={'flex justify-between py-28'}>
-        <h1 className={'text-4xl uppercase tracking-widest font-bold text-white'}>Todo</h1>
+        <h1 className={'text-4xl font-bold uppercase tracking-widest text-white'}>Todo</h1>
         <ModeToggle />
       </div>
 
-      <Input className={'h-12 mb-10 text-base'} placeholder={'Create a new todo...'} />
+      <Input className={'mb-10 h-12 text-base'} placeholder={'Create a new todo...'} />
 
-      <div className={'border rounded-lg overflow-hidden mb-16'}>
+      <div className={'mb-16 overflow-hidden rounded-lg border'}>
         <Table className={'bg-background text-base'}>
-          <TableCaption className={'text-left p-4 m-0'}>2 items left</TableCaption>
+          <TableCaption className={'m-0 p-4  text-left'}>
+            <div className={'flex flex-wrap items-center justify-center gap-5 sm:justify-between'}>
+              <p>2 items left</p>
+
+              <ul className={'flex gap-5'}>
+                <li>All</li>
+                <li>Active</li>
+                <li>Completed</li>
+              </ul>
+
+              <Button variant={'ghost'}>Clear Completed</Button>
+            </div>
+          </TableCaption>
           <TableBody>
             <TableRow className={'flex items-center'}>
-              <TableCell className={'w-max flex'}>
-                <Toggle className={'w-6 h-6'} size={'sm'} variant='outline' aria-label='Toggle italic'></Toggle>
+              <TableCell className={'flex w-max'}>
+                <Toggle className={'h-6 w-6'} size={'sm'} variant='outline' aria-label='Toggle italic'></Toggle>
               </TableCell>
               <TableCell className={'w-full'}>Some todo text...</TableCell>
             </TableRow>
             <TableRow className={'flex items-center'}>
-              <TableCell className={'w-max flex'}>
-                <Toggle className={'w-6 h-6'} size={'sm'} variant='outline' aria-label='Toggle italic'></Toggle>
+              <TableCell className={'flex w-max'}>
+                <Toggle className={'h-6 w-6'} size={'sm'} variant='outline' aria-label='Toggle italic'></Toggle>
               </TableCell>
               <TableCell className={'w-full'}>Some todo text...</TableCell>
             </TableRow>
